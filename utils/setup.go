f20bf8b65ea30f339218eee674688c0a03526b77
Justin Schwartzbeck <jusschwa@green-forest.austin.rr.com>
2022-07-14 21:31:05 -0500

    Clone playbooks

--- utils/setup.go
+++ utils/setup.go
@@ -1,7 +1,38 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/go-git/go-git/v5"
+)
+
+const playbookGit = "https://github.com/e2guardian-angel/guardian-playbook.git"
 
 func Setup(host string, port int16) {
-	fmt.Println("Time to set up host", host, "on port", port)
+
+	var guardianHome string = os.Getenv("GUARDIAN_HOME")
+	var homePath string
+	var playbookDirs string
+	if guardianHome != "" {
+		homePath = path.Join(guardianHome)
+	} else {
+		homePath = path.Join(os.Getenv("HOME"), ".guardian")
+	}
+	playbookDirs = path.Join(homePath, "playbooks")
+
+	os.RemoveAll(playbookDirs)
+	os.MkdirAll(playbookDirs, 0o755)
+
+	fmt.Printf("Cloning playbooks into \"%s\"...", playbookDirs)
+	_, err := git.PlainClone(playbookDirs, false, &git.CloneOptions{
+		URL:      playbookGit,
+		Progress: os.Stdout,
+	})
+
+	if err != nil {
+		fmt.Println("Failed to clone playbooks: ", err)
+	}
+
 }
