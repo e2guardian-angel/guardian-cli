4b3ee783a3304bd5f1c7bad9502501c953e18fff
Justin Schwartzbeck <jusschwa@debian-dev>
2022-07-21 21:39:50 -0500

    Run ansible playbook

--- utils/setup.go
+++ utils/setup.go
@@ -4,67 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path"
 
 	"github.com/go-git/go-git/v5"
 )
 
 const playbookGit = "https://github.com/e2guardian-angel/guardian-playbook.git"
-
-func getAnsibleDir() string {
-	return path.Join(GuardianConfigHome(), "ansible")
-}
-
-func getAnsibleCfgFile() string {
-	return path.Join(getAnsibleDir(), "ansible.cfg")
-}
-
-func getAnsibleHostsFile() string {
-	return path.Join(getAnsibleDir(), "hosts.yml")
-}
-
-func initAnsibleDir(config Configuration) error {
-
-	ansibleDir := getAnsibleDir()
-
-	_, err := os.Stat(ansibleDir)
-	if os.IsNotExist(err) {
-		os.MkdirAll(ansibleDir, 0o755)
-	}
-
-	ansibleCfgFilename := getAnsibleCfgFile()
-	// Generate an ansible config based on the current config
-	cfgFile, err := os.Create(ansibleCfgFilename)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-	defer cfgFile.Close()
-
-	// Write ssh options
-	sshOptionsLine := fmt.Sprintf("ssh_args = -o UserKnownHostsFile=%s -i %s\n", getKnownHostsFile(), getPrivateKeyFilename())
-	cfgFile.WriteString(sshOptionsLine)
-	// Write inventory line
-	inventoryLine := fmt.Sprintf("inventory = %s\n", getAnsibleHostsFile())
-	cfgFile.WriteString(inventoryLine)
-
-	// Create hosts file
-	hostFileName := getAnsibleHostsFile()
-	hostsFile, err := os.Create(hostFileName)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-	defer hostsFile.Close()
-
-	for _, host := range config.Hosts {
-		hostsFile.WriteString(fmt.Sprintf("[%s]\n", host.Name))
-		hostsFile.WriteString(fmt.Sprintf("%s:%d\n", host.Address, host.Port))
-		hostsFile.WriteString("")
-	}
-
-	return nil
-}
 
 func Setup(name string) int {
 
@@ -84,21 +30,16 @@ func Setup(name string) int {
 		return -1
 	}
 
-	err = initAnsibleDir(config)
-	if err != nil {
-		return -1
-	}
-
-	playbookDirs := path.Join(GuardianConfigHome(), "playbooks")
+	playbookDir := path.Join(GuardianConfigHome(), "playbooks")
 
 	/*
 	 * TODO: instead of wiping the directory and re-cloning, just do a git pull
 	 */
-	os.RemoveAll(playbookDirs)
-	os.MkdirAll(playbookDirs, 0o755)
+	os.RemoveAll(playbookDir)
+	os.MkdirAll(playbookDir, 0o755)
 
-	log.Printf("Cloning playbooks into \"%s\"...\n", playbookDirs)
-	_, err = git.PlainClone(playbookDirs, false, &git.CloneOptions{
+	log.Printf("Cloning playbooks into \"%s\"...\n", playbookDir)
+	_, err = git.PlainClone(playbookDir, false, &git.CloneOptions{
 		URL:      playbookGit,
 		Progress: os.Stdout,
 	})
@@ -108,7 +49,41 @@ func Setup(name string) int {
 		return -1
 	}
 
+	// Create hosts file
+	hostFileName := path.Join(playbookDir, "hosts.yml")
+	hostsFile, err := os.Create(hostFileName)
+	if err != nil {
+		log.Fatal("Failed creating inventory file: ", err)
+		return -1
+	}
+	defer hostsFile.Close()
+
+	// Write a host entry for this host
+	hostsFile.WriteString(fmt.Sprintf("[%s]\n", target.Name))
+	hostsFile.WriteString(fmt.Sprintf("%s:%d\n", target.Address, target.Port))
+	hostsFile.WriteString("")
+
+	/*fmt.Println("A password will be needed for sudo access.")
+	password, err := getUserCredentials()
+	if err != nil {
+		log.Fatal("Error fetching password: ", err)
+		return -1
+	}*/
+
 	log.Printf("Executing playbook on target host \"%s\"...\n", target.Name)
+
+	sshOptionsLine := fmt.Sprintf("-o UserKnownHostsFile=%s -i %s\n", getKnownHostsFile(), getPrivateKeyFilename())
+	cmd := exec.Command("ansible-playbook", "-i", hostFileName, "-e", fmt.Sprintf("\"home_dir=%s\"", target.HomePath),
+		"--ssh-extra-args", sshOptionsLine, "-u", target.Username, "-K", "site.yml")
+	cmd.Dir = playbookDir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println(err.Error())
+		return -1
+	}
 
 	return 0
 
