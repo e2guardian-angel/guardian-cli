f9b740d7594323d5bfecf342f1d37595045e1217
Justin Schwartzbeck <jusschwa@debian-dev>
2022-07-25 16:35:43 -0500

    cleanup main

--- main.go
+++ main.go
@@ -11,35 +11,35 @@ import (
 var CLI struct {
 	Target struct {
 		Add struct {
-			Name       string `arg name:"name" help:"Name to refer to target host"`
-			Host       string `arg name:"host" help:"Target host address for install" type:"ip/hostname"`
-			Username   string `arg name:"username" help:"Username for SSH login"`
-			Port       uint16 `default:22 help:"SSH port"`
-			NoPassword bool   `default:false help:"Don't use password auth for SSH key exchange"`
-			HomePath   string `help:"Custom home path on remote target installation"`
-		} `cmd help:"Add a target host for installation"`
+			Name       string `arg:"" name:"name" help:"Name to refer to target host" required:"true"`
+			Host       string `arg:"" name:"host" help:"Target host address for install" type:"ip/hostname" required:"true"`
+			Username   string `arg:"" name:"username" help:"Username for SSH login" required:"true"`
+			Port       uint16 `name:"port" help:"SSH port" default:"22"`
+			NoPassword bool   `name:"no-password" help:"Don't use password auth for SSH key exchange" default:"false"`
+			HomePath   string `name:"home-path" help:"Custom home path on remote target installation"`
+		} `cmd:"" help:"Add a target host for installation" required:"true"`
 		Update struct {
-			Name       string `arg name:"name" help:"Name of target host to update"`
-			Host       string `arg name:"host" help:"Target host address for install" type:"ip/hostname"`
-			Username   string `arg name:"username" help:"Username for SSH login"`
-			Port       uint16 `default:22 help:"SSH port"`
-			NoPassword bool   `default:false help:"Don't use password auth for SSH key exchange"`
+			Name       string `arg:"" name:"name" help:"Name of target host to update" required:"true"`
+			Host       string `arg:"" name:"host" help:"Target host address for install" type:"ip/hostname" required:"true"`
+			Username   string `arg:"" name:"username" help:"Username for SSH login" required:"true"`
+			Port       uint16 `name:"port" help:"SSH port" default:"22"`
+			NoPassword bool   `name:"no-password" help:"Don't use password auth for SSH key exchange" default:"false"`
 			HomePath   string `help:"Custom home path on remote target installation"`
-		} `cmd help:"Updates a target host for installation"`
+		} `cmd:"" help:"Updates a target host for installation"`
 		Delete struct {
-			Name string `arg name:"name" help:"Name of target host to delete"`
-		} `cmd help:"Deletes a target host"`
+			Name string `arg:"" name:"name" help:"Name of target host to delete"`
+		} `cmd:"" help:"Deletes a target host"`
 		Setup struct {
-			Name string `arg name:"name" help:"Target to select for setup"`
-		} `cmd help:"Setup dependencies on host"`
+			Name string `arg:"" name:"name" help:"Target to select for setup"`
+		} `cmd:"" help:"Setup dependencies on host"`
 		List struct {
-		} `cmd help:"List configured target hosts"`
+		} `cmd:"" help:"List configured target hosts"`
 		Reset struct {
-		} `cmd help:"Reset SSH and clear all hosts"`
+		} `cmd:"" help:"Reset SSH and clear all hosts"`
 		Test struct {
-			Name string `arg name:"name" help:"Name of target host to update"`
-		} `cmd help:"Run test ssh command"`
-	} `cmd help:"Operations on target hosts"`
+			Name string `arg:"" name:"name" help:"Name of target host to update"`
+		} `cmd:"" help:"Run test ssh command"`
+	} `cmd:"" help:"Operations on target hosts"`
 }
 
 func main() {
@@ -48,8 +48,7 @@ func main() {
 	switch ctx.Command() {
 	case "target add <name> <host> <username>":
 		code = utils.AddHost(CLI.Target.Add.Name, CLI.Target.Add.Host, CLI.Target.Add.Port, CLI.Target.Add.Username, CLI.Target.Add.NoPassword, CLI.Target.Add.HomePath)
-		break
-	case "target update <name> <host>":
+	case "target update <name> <host> <username>":
 		host := utils.Host{
 			Name:     CLI.Target.Update.Name,
 			Address:  CLI.Target.Update.Host,
@@ -59,22 +58,17 @@ func main() {
 		code = utils.UpdateHost(CLI.Target.Update.Name, host, CLI.Target.Update.NoPassword)
 	case "target setup <name>":
 		code = utils.Setup(CLI.Target.Setup.Name)
-		break
 	case "target delete <name>":
 		code = utils.DeleteHost(CLI.Target.Delete.Name)
-		break
 	case "target list":
 		code = utils.ListHosts()
-		break
 	case "target reset":
 		code = utils.ResetSsh()
-		break
 	case "target test <name>":
 		code = utils.TestSshCommand(CLI.Target.Test.Name)
 	default:
 		log.Fatal("Unknown command. Use '--help' to get a list of valid commands.")
 		code = -1
-		break
 	}
 
 	os.Exit(code)
